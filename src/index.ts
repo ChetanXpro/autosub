3908bb29c2ae30ef9e06581632888abc720ced1f
Chetan baliyan <chetanbaliyan10@gmail.com>
2024-02-15 20:42:22 +0530

    feat: change example video

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { addSubtitles } from './services/video'
 dotenv.config()
 
 // enter absolute path of the video file
-const videoPath = '/Users/chetan/Developer/autosub/assets/demo.mp4'
+const videoPath = '/Users/chetan/Developer/autosub/assets/sam-interview.mp4'
 
 const subtitlePath = videoPath.replace('.mp4', '.wav.srt')
 const outputVideoPath = videoPath.split('/').slice(0, -1).join('/') + '/output.mp4'
